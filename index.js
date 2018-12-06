c078d68ab71e19ccaaa1edc0080de24ab5e45c3a
fauzan_bintang <obin.aji@gmail.com>
2018-12-06 16:49:51 +0700

    done

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const fs = require('fs');
 const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
-  // Code here
+  fs.readFile(parent_file, function(err, data) {
+    let parent_data = JSON.parse(data)
+    parent_data.forEach(parent => {
+      parent.children = []
+    });
+    fs.readFile(children_file, function(err, data) {
+      let children_data = JSON.parse(data)
+      sleep.sleep(2)
+      parent_data.forEach(parent => {
+        children_data.forEach(child => {
+          child.family === parent.last_name ? parent.children.push(child.full_name): false
+        });
+      });
+      sleep.sleep(2)
+      console.log(parent_data);
+    })
+  })
+   
 }
 
 match_data('./parents.json', './children.json')
