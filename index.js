ab0d83494ed501b40c02db64eac6a116335613c7
ChristianHS23 <christian.sihotang23@gmail.com>
2018-12-06 17:24:08 +0700

    callback-multiple-file done

--- index.js
+++ index.js
@@ -3,6 +3,35 @@ const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
   // Code here
+  fs.readFile(parent_file, "utf8", (err,data) => {
+    if(err) throw err
+    sleep.sleep(5)
+    parentData = JSON.parse(data)
+   
+    fs.readFile(children_file, "utf8", (err,data) => {
+      if(err) throw err
+
+      childrenData = JSON.parse(data)
+    
+      // console.log(parentData);
+      // console.log(childrenData);
+      let result=[]
+      for(let i = 0 ; i < parentData.length ; i++){
+        parentData[i].children = []
+        for(let j = 0 ; j < childrenData.length; j++){
+          if(childrenData[j].family == parentData[i].last_name){
+            parentData[i].children.push(childrenData[j])
+          }
+        }
+        result.push(parentData[i])
+      }
+      result.forEach( e =>{
+        console.log(e)
+      })
+    
+    })
+
+  })
 }
 
 match_data('./parents.json', './children.json')
