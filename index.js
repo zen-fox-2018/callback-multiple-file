11810c5fa682e5e331c71bdaf0e818e4e9584bab
kangman53 <hermanboy53@gmail.com>
2018-12-06 19:31:12 +0700

    Revert "Herman Yulianto callback multiple file"
    This reverts commit 4c831e24c9b46bc5a5570d75b0285d90874abf71.

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 const fs = require('fs');
 const sleep = require('sleep');
 
-
 function match_data(parent_file, children_file) {
   // Code here
-  fs.readFile(parent_file, 'utf8', (err, parents) => {
-    // sleep.sleep(5);
-    let parents_data = JSON.parse(parents);
-    if (err) throw err;
-    fs.readFile(children_file, 'utf8', (err, childrens) => {
-      if (err) throw err;
-      const childrens_data = JSON.parse(childrens);
-      parents_data.forEach( p => {
-        p.children = [];
-        childrens_data.forEach( c => {
-          if (p.last_name === c.family) {
-            p.children.push(c.full_name);
-          }
-        })
-      });
-      console.log(parents_data);
-    });
-  });
-
-
 }
 
 match_data('./parents.json', './children.json')
