a60fb532b4a60d6709d9a35e2d12a64e7b2b67b3
irsyahms <irsyahmardiahs@gmail.com>
2018-12-06 14:03:54 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const sleep = require('sleep');
+
+function match_data(parent_file, children_file) {
+  // Code here
+}
+
+match_data('./parents.json', './children.json')
+console.log("Notification : Data sedang diproses !");
