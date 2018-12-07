2991d098aeb541c6479ede322e4fb1892a94ad45
kangman53 <hermanboy53@gmail.com>
2018-12-07 10:50:02 +0700

    Herman Yulianto callback multiple file refactor

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
 const sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
+function match_data(parent_file, children_file, callback) {
   // Code here
   readFile(parent_file, (err, parents) => {
     // sleep.sleep(5);
     if (err) {
-      console.log(err);
+      callback(err, null);
+      // console.log(err);
     } else {
       let parents_data = JSON.parse(parents);
       readFile(children_file, (err, childrens) => {
         if (err) {
-          console.log(err);
+          callback(err, null);
         } else {
           const childrens_data = JSON.parse(childrens);
+          let matchData = [];
           parents_data.forEach( p => {
             p.children = [];
             childrens_data.forEach( c => {
@@ -21,9 +23,10 @@ function match_data(parent_file, children_file) {
                 p.children.push(c.full_name);
               }
             })
+            matchData.push(p);
           });
+          callback(null, matchData);
         }
-        console.log(parents_data);
       });
     }
   });
@@ -32,11 +35,17 @@ function match_data(parent_file, children_file) {
  function readFile(file, callback) {
    fs.readFile(file, 'utf8', (err, data) => {
      if (err) {
-       callback(err);
+       callback(err, null);
      } else {
-       callback(data);
+       callback(null, data);
      }
    });
  }
-match_data('./parents.json', './children.json')
+match_data('./parents.json', './children.json', (err, matchData) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(matchData);
+  }
+})
 console.log("Notification : Data sedang diproses !");
