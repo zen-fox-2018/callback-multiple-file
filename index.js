3f590d9b7e634170c3e7a8e52141cd9d43209a03
fauzan_bintang <obin.aji@gmail.com>
2018-12-07 10:39:54 +0700

    done

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 const fs = require('fs');
 const sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
-  fs.readFile(parent_file, function(err, data) {
-    let parent_data = JSON.parse(data)
-    parent_data.forEach(parent => {
-      parent.children = []
-    });
-    fs.readFile(children_file, function(err, data) {
-      let children_data = JSON.parse(data)
-      sleep.sleep(2)
-      parent_data.forEach(parent => {
-        children_data.forEach(child => {
-          child.family === parent.last_name ? parent.children.push(child.full_name): false
-        });
-      });
-      sleep.sleep(2)
-      console.log(parent_data);
-    })
+function readFile(file, cb) {
+  fs.readFile(file, (err, data) => {
+    err ? cb({msg: 'err readFile', err: err}): cb(null, data)
   })
-   
 }
 
-match_data('./parents.json', './children.json')
+function match_data(parent_file, children_file, cb) {
+  readFile(parent_file, (err, data) => {
+    if (err) {
+      cb(err)
+    } else {
+      let parentData = JSON.parse(data)
+      readFile(children_file, (err, data) => {
+        if (err) {
+          cb(err)
+        } else {
+          let childrenData = JSON.parse(data)
+          sleep.sleep(2)
+          parentData.forEach(parent => {
+            parent.children = []
+            childrenData.forEach(child => {
+              child.family === parent.last_name ? parent.children.push(child.full_name): false
+            });
+          });
+          sleep.sleep(2)
+          cb(null, parentData)
+        }
+      })
+    }
+  })
+}
+
+match_data('./parents.json', './children.json', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+})
 console.log("Notification : Data sedang diproses !");
